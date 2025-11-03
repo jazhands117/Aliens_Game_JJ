829514fdb051c5b2bb5c94caa5a99d801aba26ca
Jay Johnson <jay.johnson@students.snow.edu>
2025-11-02 20:28:52 -0700

    Final commit. No boss fight

--- Lab08/GameDesign/GameProgress.cs
+++ Lab08/GameDesign/GameProgress.cs
@@ -2,10 +2,10 @@ namespace Lab08.GameDesign
 {
     public enum GameProgress
     {
-        None = 0,
-        MechActivated = 1,
-        BossDiscovered = 2,
-        BossDefeated = 3,
-        StoryCompleted = 4
+        None,
+        MechActivated,
+        BossDiscovered,
+        BossDefeated,
+        StoryCompleted
     }
 }
